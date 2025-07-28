a44dde1db61d01cb49d05d3c59fb5808ee65a568
TaufiqDatau <taufiqhdatau@gmail.com>
2025-07-28 20:43:22 +0700

    add chat bubble to homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Input from "/src/component/textInput.tsx";
+import Bubble from "/src/component/chatBubbles.tsx";
 
 function HomePage() {
   return (
     <div>
+      <Bubble />
       <Input />
     </div>
   );
